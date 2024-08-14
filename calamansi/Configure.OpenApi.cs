d72815437233b97c2c8e5100b48906a00af26e34
Robin Michael <Siliconrob@siliconheaven.net>
2024-08-14 02:23:50 -0500

    Always show swagger

--- calamansi/Configure.OpenApi.cs
+++ calamansi/Configure.OpenApi.cs
@@ -6,17 +6,12 @@ public class ConfigureOpenApi : IHostingStartup
 {
     public void Configure(IWebHostBuilder builder) => builder
         .ConfigureServices((context, services) => {
-            if (context.HostingEnvironment.IsDevelopment())
-            {
-                services.AddEndpointsApiExplorer();
-                services.AddSwaggerGen();
-
-                services.AddServiceStackSwagger();
-                //services.AddBasicAuth<Data.ApplicationUser>();
-                //services.AddJwtAuth();
-            
-                services.AddTransient<IStartupFilter,StartupFilter>();
-            }
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddServiceStackSwagger();
+            //services.AddBasicAuth<Data.ApplicationUser>();
+            //services.AddJwtAuth();
+            services.AddTransient<IStartupFilter,StartupFilter>();
         });
 
     public class StartupFilter : IStartupFilter
