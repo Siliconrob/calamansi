756b8f71ba72cc9f2a1fa7600f59714f4444e32e
Robin Michael <Siliconrob@siliconheaven.net>
2024-09-19 14:17:58 -0500

    Update to latest

--- calamansi.Tests/RequestProxyTest.cs
+++ calamansi.Tests/RequestProxyTest.cs
@@ -1,6 +1,6 @@
-﻿using calamansi.ServiceInterface;
-using calamansi.ServiceInterface.Utils;
+﻿using calamansi.ServiceInterface.Utils;
 using NUnit.Framework;
+using NUnit.Framework.Legacy;
 using ServiceStack.Caching;
 
 namespace calamansi.Tests;
@@ -18,9 +18,9 @@ public class RequestProxyTest
         foreach (var index in Enumerable.Range(0, 2))
         {
             var countries = await RequestProxy.GetCountries(cache);
-            Assert.IsNotEmpty(countries);
+            ClassicAssert.IsNotEmpty(countries);
             var country = countries.FirstOrDefault();
-            Assert.NotNull(country);
+            ClassicAssert.NotNull(country);
             await TestContext.Out.WriteLineAsync(country?.Cca2);
         }
     }
@@ -29,7 +29,7 @@ public class RequestProxyTest
     public async Task ByRegionsTest()
     {
         var countries = await RequestProxy.GetCountries(cache);
-        Assert.IsNotEmpty(countries);
+        ClassicAssert.IsNotEmpty(countries);
         var first = countries.FirstOrDefault();
         await TestContext.Out.WriteLineAsync(first?.Name);
         var dict = DictionaryBuilder.ByRegions(countries);
@@ -37,14 +37,14 @@ public class RequestProxyTest
         {
             await TestContext.Out.WriteLineAsync($"{key}:{value.Count}");
         }
-        Assert.True(dict.ContainsKey("Americas".ToLowerInvariant()));
+        ClassicAssert.True(dict.ContainsKey("Americas".ToLowerInvariant()));
     }    
     
     [Test]
     public async Task ByLanguagesTest()
     {
         var countries = await RequestProxy.GetCountries(cache);
-        Assert.IsNotEmpty(countries);
+        ClassicAssert.IsNotEmpty(countries);
         var first = countries.FirstOrDefault();
         await TestContext.Out.WriteLineAsync(first?.Name);
         var dict = DictionaryBuilder.ByLanguages(countries);
@@ -52,14 +52,14 @@ public class RequestProxyTest
         {
             await TestContext.Out.WriteLineAsync($"{key}:{value.Count}");
         }
-        Assert.True(dict.ContainsKey("eng".ToLowerInvariant()));
+        ClassicAssert.True(dict.ContainsKey("eng".ToLowerInvariant()));
     }
     
     [Test]
     public async Task OrderByTest()
     {
         var countries = await RequestProxy.GetCountries(cache);
-        Assert.IsNotEmpty(countries);
+        ClassicAssert.IsNotEmpty(countries);
         var first = countries.FirstOrDefault();
         await TestContext.Out.WriteLineAsync(first.Name);
         countries = countries.OrderByDynamic("cca2").ToList();
@@ -72,10 +72,10 @@ public class RequestProxyTest
     public async Task FlattenTest()
     {
         var countries = await RequestProxy.GetCountries(cache);
-        Assert.IsNotEmpty(countries);
+        ClassicAssert.IsNotEmpty(countries);
         var matches = countries.SearchAll("Georgia");
-        Assert.IsNotEmpty(matches);
-        Assert.AreEqual(matches.Count, 2);
+        ClassicAssert.IsNotEmpty(matches);
+        ClassicAssert.AreEqual(matches.Count, 2);
         foreach (var match in matches)
         {
             await TestContext.Out.WriteLineAsync(match.Name);   
